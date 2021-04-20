9594b956497e0cfab122708f9d41f9ca5f4f05a9
tdn <2387083001@qq.com>
2021-04-20 21:49:52 +0800

    初始化Nuxt.js项

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 张泽涛
+ * @LastEditors: 张泽涛
+ * @LastEditTime: 2021-04-20 19:46:16
+-->
+<template>
+  <div>
+      <h1>about Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'aboutPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 张泽涛
+ * @LastEditors: 张泽涛
+ * @LastEditTime: 2021-04-20 19:47:15
+-->
+<template>
+  <div>
+    <h1>Hello world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style>
+
+</style>
