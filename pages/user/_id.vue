8b2a0eee746b3033e8d72494453b31619c389141
tdn <2387083001@qq.com>
2021-04-21 23:08:58 +0800

    动态路由

---
+++ pages/user/_id.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 张泽涛
+ * @LastEditors: 张泽涛
+ * @LastEditTime: 2021-04-21 23:08:26
+-->
+<template>
+  <div>
+      <h1>User Page</h1>
+      <p>{{$route.params.id}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'UserPage'
+}
+</script>
+
+<style>
+
+</style>
