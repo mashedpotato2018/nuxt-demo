3e37ffe1e34d9e519bddcf6bbccf860866a9c35a
tdn <2387083001@qq.com>
2021-04-21 22:43:30 +0800

    router

---
+++ pages/user/abc.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 张泽涛
+ * @LastEditors: 张泽涛
+ * @LastEditTime: 2021-04-21 22:38:31
+-->
+<template>
+  <div>
+      <h1>UserAbc page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "UserAbc",
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 张泽涛
+ * @LastEditors: 张泽涛
+ * @LastEditTime: 2021-04-21 22:39:08
+-->
+<template>
+  <div>
+      <h1>userPage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "userPage",
+}
+</script>
+
+<style>
+
+</style>
