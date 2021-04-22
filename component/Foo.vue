3a7a7e694bdc161ad12ac22d0e8b77ade83bb56b
tdn <2387083001@qq.com>
2021-04-22 19:35:20 +0800

    上下文对象

--- component/Foo.vue
+++ component/Foo.vue
@@ -3,13 +3,13 @@
  * @version: 1.0
  * @Author: 张泽涛
  * @LastEditors: 张泽涛
- * @LastEditTime: 2021-04-22 19:00:50
+ * @LastEditTime: 2021-04-22 19:10:39
 -->
 <template>
   <div>
     <h1>FooPage</h1>
     <ul>
-      <li v-for="item in posts" :key="item.id">{{ item.title }}</li>
+      <li v-for="item in posts" :key="item.id"><nuxt-link :to="'article/'+ item.id">{{ item.title }}</nuxt-link></li>
     </ul>
   </div>
 </template>
