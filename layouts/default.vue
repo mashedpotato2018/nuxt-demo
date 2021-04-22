bd1b7215a01947aa6375a33626cea7977d69317b
tdn <2387083001@qq.com>
2021-04-22 18:39:12 +0800

    布局组件

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,18 +3,19 @@
  * @version: 1.0
  * @Author: 张泽涛
  * @LastEditors: 张泽涛
- * @LastEditTime: 2021-04-22 01:30:23
+ * @LastEditTime: 2021-04-22 01:25:08
 -->
 <template>
   <div>
-    <h1>Hello world!</h1>
+      <h1>layouts/default.vue组件</h1>
+      <!-- 页面出口，类似于子路由 -->
+      <nuxt/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
-  layout: 'foo'
+    name: 'LayoutDefault'
 }
 </script>
 
--- layouts/foo.vue
+++ layouts/foo.vue
@@ -3,18 +3,18 @@
  * @version: 1.0
  * @Author: 张泽涛
  * @LastEditors: 张泽涛
- * @LastEditTime: 2021-04-22 01:30:23
+ * @LastEditTime: 2021-04-22 01:30:38
 -->
 <template>
   <div>
-    <h1>Hello world!</h1>
+      <h1>layouts/foo.vue 组件</h1>
+      <nuxt/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
-  layout: 'foo'
+    name: 'LayoutFoo'
 }
 </script>
 
